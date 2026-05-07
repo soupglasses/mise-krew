49c405114856112d2d4cc1eb55eddee0249f5f51
Sofie Finnes <sofie+git@finnes.dev>
2026-05-07 02:07:49 +0200

    chore: prepare release 2.1.0

--- metadata.lua
+++ metadata.lua
@@ -2,9 +2,10 @@
 -- Backend plugin metadata and configuration
 -- Documentation: https://mise.jdx.dev/backend-plugin-development.html
 
+-- luacheck: ignore 131
 PLUGIN = {
     name = "krew",
-    version = "2.0.0",
+    version = "2.1.0",
     description = "A mise backend plugin for krew tools with version support",
     author = "soupglasses",
     homepage = "https://github.com/soupglasses/mise-krew",
